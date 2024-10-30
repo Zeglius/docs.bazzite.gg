f1408237d8dd5dece4a82581f56f4b6acb95cd9c
Zeglius <33781398+Zeglius@users.noreply.github.com>
2024-10-30 01:21:02 +0000

    chore(utils): Fine tweak on install-deps

--- utils/install-deps.sh
+++ utils/install-deps.sh
@@ -4,25 +4,33 @@ REPO_DIR="$(git rev-parse --show-toplevel)"
 
 function echod() { echo "[DEBUG]: $*"; }
 
-brew_installs=(
+brew_pkgs=(
     uv
     just
     prettier
     rg
 )
 
+brew_pkgs_extras=( lazygit )
+
 if ! command -v brew >/dev/null; then
     echod "This script needs 'brew' to run"
     exit 1
 fi
 
-brew install ${brew_installs[@]}
+# Include extra dependencies with --extra flag
+[[ $* == --extra ]] && brew_pkgs+=(${brew_pkgs_extras[@]})
+
+echod "Installing dependencies:"
+printf -- ' - %s\n' ${brew_pkgs[@]}
+{ brew install ${brew_pkgs[@]}; } || { echod "Something went wrong..."; exit 1; }
+
 
 # Install project
 echod "Setting up python project"
 (
     cd "$REPO_DIR"
-    uv venv
+    uv sync --dev --locked
 ) || {
     echod "Error setting up python project"
     exit 1
